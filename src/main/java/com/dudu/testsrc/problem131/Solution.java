b0efd274a9046beacc5682eb0ae446f2046dd454
luzhao <zhao.lu@parcelx.io>
2019-07-26 19:24:13 +0800

    problem131

---
+++ src/main/java/com/dudu/testsrc/problem131/Solution.java
@@ -0,0 +1,80 @@
+package com.dudu.testsrc.problem131;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author zhaolu
+ * @version 1.0
+ * @datetime 7/26/2019 4:52 PM
+ * @email zhao.lu@parcelx.io
+ */
+public class Solution {
+    private List<List<List>> store;
+
+    public List<List<String>> partition(String s) {
+        if (s == null || s.length() == 0) {
+            return null;
+        }
+        int len = s.length();
+        if (store == null) {
+            store = new ArrayList<>();
+            for (int index = 0; index <= len; index++) {
+                store.add(new ArrayList<>());
+                for (int innerindex = 0; innerindex <= len; innerindex++) {
+                    store.get(index).add(new ArrayList<>());
+                }
+            }
+        }
+        List<List<String>> result = new LinkedList<>();
+        if (s.length() == 1) {
+            result.add(Stream.of(s).collect(Collectors.toList()));
+            return result;
+        } else {
+            for (int index = 1; index <= s.length(); index++) {
+                String leftStr = s.substring(0, index);
+                String rightStr = s.substring(index);
+                if (isPalindrome(leftStr)) {
+                    if(store.get(index).get(s.length()) == null || store.get(index).get(s.length()).size() == 0){
+                        store.get(index).set(s.length(), partition(rightStr));
+                    }
+                    List<List<String>> rightResult = store.get(index).get(s.length());
+                    List<String> allTemResult;
+                    List<String> perLeftResult = Stream.of(leftStr).collect(Collectors.toList());
+                    if (rightResult != null) {
+                        for (List<String> perRightResult : rightResult) {
+                            allTemResult = new LinkedList<>();
+                            allTemResult.addAll(perLeftResult);
+                            allTemResult.addAll(perRightResult);
+                            result.add(allTemResult);
+                        }
+                    } else {
+                        allTemResult = new LinkedList<>(perLeftResult);
+                        result.add(allTemResult);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    private boolean isPalindrome(String s) {
+        int len = s.length() - 1;
+        for (int index = 0; index <= len / 2; index++) {
+            if (s.charAt(index) != s.charAt(len - index)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.partition("aab");
+    }
+
+
+}
--- src/main/java/com/dudu/testsrc/tenproblem/Solution.java
+++ src/main/java/com/dudu/testsrc/tenproblem/Solution.java
@@ -6,72 +6,57 @@ import java.util.Stack;
 
 public class Solution {
     public boolean isMatch(String s, String p) {
-
         //思路是分别为两个字符串创建两个栈
         Stack<Character> sStack = new Stack<>();
         Stack<Character> pStack = new Stack<>();
 
-        Set<Character> sSet = new HashSet<Character>(){{
-           for(char c='a'; c<='z'; c++){
-               add(c);
-           }
+        Set<Character> sSet = new HashSet<Character>() {{
+            for (char c = 'a'; c <= 'z'; c++) {
+                add(c);
+            }
         }};
         Set<Character> pSet = new HashSet<Character>(sSet);
         pSet.add('.');
         pSet.add('*');
 
         //然后把字符串每个字符都放到栈里
-        for(int i=0; i<s.length(); i++){
-            if(sSet.contains(s.charAt(i))){
+        for (int i = 0; i < s.length(); i++) {
+            if (sSet.contains(s.charAt(i))) {
                 sStack.add(s.charAt(i));
-            }else{
+            } else {
                 return false;
             }
         }
-        for(int j=0; j<p.length(); j++){
-            if(pSet.contains(p.charAt(j))){
+        for (int j = 0; j < p.length(); j++) {
+            if (pSet.contains(p.charAt(j))) {
                 pStack.add(p.charAt(j));
-            }else{
+            } else {
                 return false;
             }
         }
-
-
-
         //是否是循环状态
         boolean isCycle = false;
         //ptChar 用来存放cycle的char
-        Character sChar, pChar, ptChar=null;
-        while(!pStack.isEmpty()&&!sStack.isEmpty()){
+        Character sChar, pChar, ptChar = null;
+        while (!pStack.isEmpty() && !sStack.isEmpty()) {
             sChar = sStack.pop();
-            if(isCycle){
-                if(ptChar!=null){
+            if (isCycle) {
+                if (ptChar != null) {
 
-                    while (!sStack.isEmpty()&&(sStack.peek()==ptChar||ptChar=='.')){
+                    while (!sStack.isEmpty() && (sStack.peek() == ptChar || ptChar == '.')) {
                         sStack.pop();
                     }
 
-                }else{
+                } else {
                     pChar = pStack.pop();
 
-                    if(pChar=='*'){
+                    if (pChar == '*') {
                         isCycle = true;
                     }
                     ptChar = pChar;
                 }
             }
-
-
-
-
-
-
-
-
         }
-
-
+        return true;
     }
-
-
 }
